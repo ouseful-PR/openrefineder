4cef25a4ca6077536c5f49cafb531499fbcad96e
Tony Hirst <tony.hirst@gmail.com>
2019-01-09 20:49:31 +0000

    Running with the new jupyter-server-proxy

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name="nbopenrefineproxy",
-    version='0.0.1',
-    url="https://github.com/betatim/nbopenrefineproxy",
-    author="Tim Head",
-    description="Jupyter extension to proxy OpenRefine sessions",
-    packages=setuptools.find_packages(),
-    keywords=['Jupyter'],
-    classifiers=['Framework :: Jupyter'],
-    install_requires=[
-        'notebook',
-        'nbserverproxy >= 0.3.2'
-    ],
-    package_data={'nbopenrefineproxy': ['static/*']},
+  name="jupyter-openrefine-server",
+  # py_modules rather than packages, since we only have 1 file
+  py_modules=['openrefine'],
+  entry_points={
+      'jupyter_serverproxy_servers': [
+          # name = packagename:function_name
+          'openrefine = openrefine:setup_openrefine',
+      ]
+  },
 )
