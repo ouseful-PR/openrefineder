972b087dc89c543ed2bffd290487285f7968e0a1
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 13:57:38 +0000

    Add logo to lab launcher

--- openrefine.py
+++ openrefine.py
@@ -4,5 +4,10 @@ def setup_openrefine():
   path = os.path.join(os.environ['HOME'], 'openrefine')
   return {
     'command': ['/home/jovyan/openrefine-2.8/refine', '-p', '{port}','-d',path],
-    'port': 3333
+    'port': 3333,
+    'launcher_entry': {
+        'enabled': True,
+        'icon_path': '/home/jovyan/.jupyter/open-refine-logo.svg',
+        'title': 'OpenRefine',
+    },
   }
