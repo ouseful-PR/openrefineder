34a55db41342b7d285d06554a886cafbf2b15467
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 14:16:16 +0000

    I'm not sure the launcher setting is detected?

--- openrefine.py
+++ openrefine.py
@@ -5,6 +5,7 @@ def setup_openrefine():
   return {
     'command': ['/home/jovyan/openrefine-2.8/refine', '-p', '{port}','-d',path],
     'port': 3333,
+    #The following isn't picked up?
     'launcher_entry': {
         'enabled': True,
         'icon_path': '/home/jovyan/.jupyter/open-refine-logo.svg',
